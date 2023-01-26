abfe6b658c0b3fe696fe1ecfdbb276c3dc8e05a9
nabil <nchaour6@gmail.com>
2023-01-26 12:20:23 -0500

    update runner 3 add json

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,12 +5,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+
 @CucumberOptions(
         features = ".//features",
       //  features = {".//Features/createAccount.feature",".//Features/login.feature"},
         glue = "stepDefinitions",
         dryRun = false,
-        plugin={"pretty","html:test-output.html"},
+        plugin={"pretty","html:test-output.html","json:target/cucumber.json"},
         tags= "@sanity or @regression"
 )
 public class TestRunner {
